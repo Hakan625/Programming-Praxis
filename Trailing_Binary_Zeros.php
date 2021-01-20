04adc3d4f16f9f5656ed4164ee60b0765a4a0651
Hakan Deniz <56585381+Hakan625@users.noreply.github.com>
2021-01-20 02:06:29 +0100

    Update Trailing_Binary_Zeros.php

--- Trailing_Binary_Zeros.php
+++ Trailing_Binary_Zeros.php
@@ -3,8 +3,8 @@
 * Given a positive integer, count the number of trailing zero-bits in its binary representation. 
 * 
 * For instance, 
-* 1810 = 100102, so it has 1 trailing zero-bit, and 
-* 4810 = 1100002, so it has 4 trailing zero-bits.
+* 18 = 10010, so it has 1 trailing zero-bit, and 
+* 48 = 110000, so it has 4 trailing zero-bits.
 * 
 * Your task is to write a program that counts the number of trailing zero-bits in the binary representation of a positive integer. 
 *
